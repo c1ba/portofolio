6b6ab772a4c7546bc0b611edc3c64e96d3acedeb
Alexandra Costache <costachealexandra28@gmail.com>
2023-07-06 12:35:55 +0300

    Updated projects list

--- src/pgs/projects.jsx
+++ src/pgs/projects.jsx
@@ -2,11 +2,14 @@ import React, { Component } from 'react'
 import { defaultSideBarStyling } from '../common/cssProperties'
 import { LinkText } from '../components/LinkText'
 
-const links = [{text: "Throw Trash Responsibly", link: "https://c1ba.github.io/throw_trash_responsibly/"},
-{text: "Color Scroller(Hex Code Based)(PC browser)", link: "https://friendly-wescoff-c440fd.netlify.app/"},
-{text: "Books To Read List", link: "https://c1ba.github.io/my_to_read/"},
-{text: "APEDYO (Computer Science Certificate)", link: "https://ciba.itch.io/apedyo"},
-{text: "What Philosphy Suits You Most Quiz", link: "https://c1ba.github.io/quizhub_philosophy/"}]
+const links = [
+  {text: "Portofolio (this one)", link: "https://github.com/c1ba/portofolio"},
+  {text: "Throw Trash Responsibly", link: "https://c1ba.github.io/throw_trash_responsibly/"},
+  {text: "Color Scroller(Hex Code Based)(PC browser)", link: "https://friendly-wescoff-c440fd.netlify.app/"},
+  {text: "Books To Read List", link: "https://c1ba.github.io/my_to_read/"},
+  {text: "APEDYO (Computer Science Atestate)", link: "https://ciba.itch.io/apedyo"},
+  {text: "What Philosphy Suits You Most Quiz", link: "https://c1ba.github.io/quizhub_philosophy/"},
+  {text: "Rainbowfier", link: "https://github.com/c1ba/rainbowfier-react"}];
 
 export default class Projects extends Component {
   render() {
